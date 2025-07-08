c53237e5eccd99e53e0676314bb785f72251be1b
finnr <finn.reinhardt@gmx.de>
2025-07-08 20:18:34 +0200

    add user_stream_data folder Creation
    ;

--- backend/socket_server.ts
+++ backend/socket_server.ts
@@ -10,11 +10,16 @@ export default async function start_socket_server(io: Server) {
     console.log("socket id connected: ", socket.id);
 
     const { id } = socket;
-    const folder = p.join(base_path, id);
-    mkdirSync(folder);
+    const user_folder = p.join(base_path, id);
+
+    mkdirSync(user_folder);
+
+    const user_stream_folder = p.join(user_folder, id + "_stream_data");
+
+    mkdirSync(user_stream_folder);
 
     socket.on("disconnect", (_) => {
-      rmdirSync(folder, { recursive: true });
+      rmdirSync(user_folder, { recursive: true });
       console.log("Client disconnected:", socket.id);
     });
   });
