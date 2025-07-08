5b016c0eb0e9b8ea8855fa1a1bc8d4e3a62677fd
finnr <finn.reinhardt@gmx.de>
2025-07-08 20:30:42 +0200

    add dotenv support

--- backend/server.ts
+++ backend/server.ts
@@ -3,16 +3,17 @@ import { createServer } from "http";
 import * as p from "path";
 import { Server } from "socket.io";
 import start_socket_server from "./socket_server";
+import "dotenv/config";
 
 const port = 3000;
 const host = "localhost";
-const html_path = "./index.html";
+const html_name = "index.html";
 
 const app = express();
 const server = createServer(app);
 
 app.get("/", (req, res) => {
-  res.sendFile(p.join(__dirname, "../../index.html"));
+  res.sendFile(p.join(__dirname, `../../${html_name}`));
 });
 
 start_socket_server(new Server(server));
