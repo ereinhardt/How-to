a2c1a712df136fb661637a51645b3307d8e7ba63
Finnr <finn.reinhardt@gmx.de>
2025-08-17 20:38:22 +0200

    start making ai wrapper

---
+++ ai/ai.ts
@@ -0,0 +1,25 @@
+import * as p from "path";
+import { save_accesing_env_field } from "../util/util";
+import { readFileSync } from "fs";
+import { GoogleGenAI } from "@google/genai";
+
+export default async function generate_question(start_question: string) {
+  const video_folder = save_accesing_env_field("VIDEOS_PATH");
+  const api_key = save_accesing_env_field("GEMINI_API_KEY");
+  const users_questions_path = p.join(
+    __dirname,
+    "../../",
+    video_folder,
+    "question_index.csv"
+  );
+  const users_csv = readFileSync(users_questions_path, { encoding: "utf8" });
+
+  const ai = new GoogleGenAI({ apiKey: api_key });
+
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "Erzähle mir ein witz über Informatiker!",
+  });
+
+  console.log(response.text);
+}
--- backend/server.ts
+++ backend/server.ts
@@ -5,6 +5,7 @@ import start_socket_server from "./socket_server";
 import "dotenv/config";
 import start_http_server from "./http_server";
 import { save_accesing_env_field } from "../util/util";
+import generate_question from "../ai/ai";
 
 const port = save_accesing_env_field("SERVER_PORT");
 const host = save_accesing_env_field("SERVER_HOST");
@@ -15,6 +16,9 @@ const server = createServer(app);
 start_http_server(app);
 start_socket_server(new Server(server));
 
-server.listen(port, () => {
+server.listen(port, async () => {
   console.log(`server running at http://${host}:${port}`);
+
+  //TEST;
+  await generate_question("");
 });
